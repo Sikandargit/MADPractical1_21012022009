1a8e9c22445e0210d475e4048204b652d1e4877a
sikandargit <sikandardamor21@gnu.ac.in>
2022-08-23 23:45:37 +0530

    prectical upto 1.11

--- src/main/kotlin/practical1.8.kt
+++ src/main/kotlin/practical1.8.kt
@@ -1,71 +1,60 @@
-fun main()
-{
-    var arr1 = arrayOf(1,2,3,4,5)
-    //joinToString
-    println(arr1.joinToString())
+import java.util.*
+fun main(args:Array<String>) {
+    fun printArray(vararg nameArray: Int) {
+        println(nameArray.joinToString())
+    }
 
-    var arr2 = arrayOf<String>("s","K")
-    //contentDeepToString
-    println(arr2.contentDeepToString())
+    fun <T> printArray(nameArray: Array<T>) {
+        println(nameArray.contentDeepToString())
+    }
 
-    var arr3 = Array(8){i:Int->i}
-    for(i in 0 until arr3.size)
-    {
-        for(j in i+1 until arr3.size)
-        {
-            if(arr3[i]<arr3[j])
-            {
-                arr3[j]=arr3[j]+arr3[i]
-                arr3[i]=arr3[j]-arr3[i]
-                arr3[j]=arr3[j]-arr3[i]
+    val array1 = arrayOf<Int>(10, 90, 60, 80, 100)
+    println("Create Array-1 using arrayOf() method:")
+    printArray(array1)
+    val array2 = Array<Int>(5) { 0 }
+    println("Create Array-2 using Array<>() method:")
+    printArray(array2)
+    val array3 = Array(7) { i: Int -> i }
+    println("Create Array-3 using Array and Lambda Function method:")
+    printArray(array3)
+    val array4 = IntArray(5)
+    println("Create Array-4 using intArray method:")
+    printArray(nameArray = array4)
+    val array5 = intArrayOf(0, 0, 0, 0, 0)
+    println("Create Array-5 using intArrayOf method:")
+    printArray(nameArray = array5)
+    var array6 = arrayOf(intArrayOf(1, 2), intArrayOf(4, 5), intArrayOf(7, 8))
+    println("Create 2-D array-6 using arrayOf() and intArrayOf()")
+    printArray(nameArray = array6)
+    var arr = arrayOfNulls<Int>(5)
+    var read = Scanner(System.`in`)
+    println("Enter array values")
+    for (i in 0..4) {
+        print("array[$i]=")
+        arr[i] = read.nextInt()
+    }
+    println("The array is")
+    printArray(arr)
+
+    println("****   With Built-in Function   ******")
+    arr.sort()
+    printArray(arr)
+
+    println("****   Without Built-in Function   ******")
+    fun sorting(arr: Array<Int>) {
+        val len = arr.size
+        println(len)
+        var temp = 0
+        for (i in 0 until len - 1) {
+            for (j in i until len - 1) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = temp
+                }
             }
         }
     }
-    println(arr3.joinToString())
-
-    var arr4 = IntArray(3)
-    //until
-    for(i in 0 until arr4.size)
-    {
-        print("a[$i]: ")
-        arr4[i]= readLine()!!.toInt()
-    }
-
-    var arr5 = intArrayOf(25,90,10,35)
-    println(arr5.joinToString())
-    //Sort
-    arr5.sort()
-    println(arr5.joinToString())
-
-    var arr6 = arrayOf(intArrayOf(1,2), intArrayOf(3,4,3))
-    for(i in 0 until arr6.size)
-    {
-        for(j in 0 until arr6[i].size)
-        {
-            print(arr6[i][j])
-        }
-        println()
-    }
-
-    //Range
-    val num = 10
-    if (num in 5..10) {
-        println("in range")
-    }
-    if(num !in 5 .. 9)
-    {
-        println("not in range")
-    }
-
-    //step
-    for (x in 1..10 step 2) {
-        print("$x ")
-    }
-    println()
-
-    //downTo
-    for (x in 9 downTo 0 step 3) {
-        print("$x ")
-    }
-    println()
+    print("After sorting without built-in function")
+    printArray(arr)
 }
